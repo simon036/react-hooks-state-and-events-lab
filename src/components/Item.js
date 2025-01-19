0a71cbd11717df555ecd141f8e67d1c0bdc56156
Simon <ndungusimon564@gmail.com>
2025-01-19 16:52:53 +0300

    complete lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,17 +3,17 @@ import React, { useState } from "react";
 
 function Item({ name, category }) {
 
-  const [isInCart, setIsInCart] = useState(false);
+const [isInCart, setIsInCart] = useState(false);
 
-  function handleAddToCartClick() {
+function handleAddToCartClick() {
     setIsInCart((isInCart) => !isInCart);
-  }
+}
 
   return (
     <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}
-        
+
       </span>
       <button
         className={isInCart ? "remove" : "add"}
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -19,20 +19,24 @@ function ShoppingList({ items }) {
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
+        <select name="filter"
+        onChange={handleCategoryChange}>
 
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
+        
         </select>
       </div>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
+          
           <Item 
           key={item.id}
           name={item.name} 
           category={item.category} />
+          
         ))}
       </ul>
     </div>
